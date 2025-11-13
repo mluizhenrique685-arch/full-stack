4e766f1ccbe975eb8f2db36f84af91a85d405b31
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-11-13 15:03:06 -0300

    mexendo no site

--- logica-programacao/pontos.js
+++ logica-programacao/pontos.js
@@ -3,5 +3,21 @@ const empate = Number(prompt("Quantos jogos seu time empatou"));
 
 const pontos = (vitoria * 3) + empate;
 
-if (pontos)
-    document.write(pontos)
+
+function mostra(mensagem){
+
+document.write(mensagem + pontos);
+
+}
+
+if (pontos > 10){
+
+  mostra (Number("Seu time está indo bem"));
+
+  }
+else if (pontos < 5){
+ 
+  mostra (Number("Seu time precisa melhorar"));
+
+}
+mostra()
