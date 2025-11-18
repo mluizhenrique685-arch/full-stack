f3e5e0c002235d99daabb39f41ad9995cb82b786
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-11-18 15:02:01 -0300

    fiz alteração no site para quando estiver um campo em vazio avisar ao usuario em um alert

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,10 +1,30 @@
+const form = document.querySelector("form")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone")
+form.addEventListener("submit", function(event) {
+event.preventDefault();
 
-function btnCadastrar(event) {
-    event.preventDefault();
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTelefone.value);
+if(inputNome.value == "") {
+    alert("Não deixe o campo Nome em vazio, digite");
+    return false;
+
 }
+if(inputEmail.value == "") {
+    alert("Não deixe o campo Email em vazio, digite");
+    return false;
+
+}
+if(inputTelefone.value == "") {
+    alert("Não deixe o campo Telefone em vazio, digite");
+    return false;
+
+}
+
+
+
+console.log("Nome", inputNome.value);
+console.log("Email", inputEmail.value);
+console.log("Telefone", inputTelefone.value);
+});
+
