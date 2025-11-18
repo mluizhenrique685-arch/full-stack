00b391d8913aa01290edf4b5dba86240527968bb
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-11-18 16:15:57 -0300

    colocando que quando você loga suas informações resetam na barra de pesquisa

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,30 +1,28 @@
 const form = document.querySelector("form")
+const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone")
 form.addEventListener("submit", function(event) {
 event.preventDefault();
 
-if(inputNome.value == "") {
-    alert("Não deixe o campo Nome em vazio, digite");
-    return false;
-
-}
-if(inputEmail.value == "") {
-    alert("Não deixe o campo Email em vazio, digite");
-    return false;
-
-}
-if(inputTelefone.value == "") {
-    alert("Não deixe o campo Telefone em vazio, digite");
+if(inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" ) {
+    alert("Digite seus dados");
     return false;
 
 }
 
 
+const li = document.createElement("li");
 
-console.log("Nome", inputNome.value);
-console.log("Email", inputEmail.value);
-console.log("Telefone", inputTelefone.value);
+li.innerHTML = `
+  <span class="contato-Nome">${inputNome.value}</span>
+  <span class="contato-Email">${inputEmail.value}</span>
+  <span class="contato-Telefone">${inputTelefone.value}</span>
+`;
+
+console.log(li)
+lista.appendChild(li)
+form.reset();
+
 });
-
