93b76494c8f45d268bc9dc5cac2de4ec22d886a8
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-11-19 16:00:52 -0300

    fazendo alterações no site

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -3,11 +3,12 @@ const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone")
+const inputEndereço = document.getElementById("endereço")
 const listaMsg = document.querySelector(".lista-msg");
 form.addEventListener("submit", function(event) {
 event.preventDefault();
 
-if(inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" ) {
+if(inputNome.value == "" || inputEmail.value == "" || inputTelefone.value == "" || inputEndereço == "" ) {
     alert("Digite seus dados");
     return false;
 
@@ -26,13 +27,19 @@ btnExcluir.className = "btn-delete";
 const li = document.createElement("li");
 
 btnExcluir.addEventListener("click", function(){
-alert("teste");
+  const confirmar = confirm("Tem certeza que deseja excluir o contato?");
+ if (confirmar){
+  li.remove();
+
+ }
+
 });
 
 li.innerHTML = `
   <span class="contato-Nome">${inputNome.value}</span>
   <span class="contato-Email">${inputEmail.value}</span>
   <span class="contato-Telefone">${inputTelefone.value}</span>
+  <span class="contato-Endereço">${inputEndereço.value}</span>
 `;
 
 console.log(li)
