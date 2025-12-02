f3998b594e4b77ee4e8bef20629b8500d258f0ef
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-12-02 16:17:39 -0300

    tentando fazer dar certo

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,9 +1,20 @@
 import express from "express";
 const app = express();
 
+    app.use(express.json());
+
+
 app.get("/", (req, res) => {
     res.send("Olá Node js")
 });
+
+app.get("/cadastros", (req, res) => {
+    res.send(cadastros)
+});
+
+
+
+
 
 export default app;
 
@@ -36,3 +47,46 @@ idade: 45,
 endereco: "Rua Central, 999"
 }
 ];
+
+//função buscar id
+
+function buscarCadastroPorId (id) {
+    return cadastros.filter((nome) => nome.id == id )
+};
+
+//função pegar a posição ou index do elemento array por id
+
+function buscarIdcadastros(id){
+            return cadastros.findIndex((nome) => nome.id == id);
+        };
+
+
+app.get("/cadastros/:id", (req, res) => {
+    let index = req.params.id;
+
+    res.json(buscarCadastroPorId(index))
+});
+// deletar id
+
+app.delete("/cadastros/:id", (req, res) => {
+   let id = req.params.id;
+   let index = buscarIdcadastros(id); 
+
+   //se não encontrar, retorna a erro
+
+   if (index === -1){
+    return res.status(404).send(`Nenhum cadastro com id ${id} encontrado`)
+   }
+
+   cadastros.splice(index, 1);
+   res.send(`Cadastro com id ${req.params.id} excluida com sucesso`)
+});
+
+//criando rota para cadastrar times
+
+app.post("/cadastros", (req, res) => {
+    cadastros.push(req.body);
+    res.status(201).send("Cadastro cadastrado com sucesso");
+
+     
+});
