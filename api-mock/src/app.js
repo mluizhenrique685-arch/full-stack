412696aa838bf296cbc40ff5d851e0a248769b67
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-12-08 14:58:03 -0300

    tentando debuga

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,10 +1,24 @@
 import express from "express";
+import conexao from "../infra/conexao.js";
 const app = express();
 
 
 
 app.get("/", (req, res) => {
-    res.send("Olá Node js")
+    res.send("Olá copa do mundo!")
+});
+
+//Buscar todas as seleções
+app.get('/selecoes', (req, res) => {
+    const sql = "select * from selecoes";
+
+    conexao.query(sql, (erro, result ) => {
+        res.json(result);
+    });
+});
+app.post('/selecoes', (req, res) => {
+    selecoes.push(req.body);
+    res.status(201).send("time cadastrado com sucesso");
 });
 
 
