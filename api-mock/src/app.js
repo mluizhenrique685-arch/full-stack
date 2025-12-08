0f850af2a2060572e34d17ff34fae2e408674e71
mluizhenrique685-arch <mluizhenrique685@gmail.com>
2025-12-08 16:18:02 -0300

    ainda em problema mas irei resolver

--- api-mock/src/app.js
+++ api-mock/src/app.js
@@ -1,28 +1,41 @@
 import express from "express";
 import conexao from "../infra/conexao.js";
+
 const app = express();
 
-
-
 app.get("/", (req, res) => {
-    res.send("Olá copa do mundo!")
+    res.send("Olá Copa do Mundo!");
 });
 
-//Buscar todas as seleções
+// Buscar todas as seleções 
 app.get('/selecoes', (req, res) => {
     const sql = "select * from selecoes";
 
-    conexao.query(sql, (erro, result ) => {
+    conexao.query(sql, (erro, result) => {
         res.json(result);
     });
 });
-app.post('/selecoes', (req, res) => {
-    selecoes.push(req.body);
-    res.status(201).send("time cadastrado com sucesso");
+
+// Buscando por ID
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+    });
 });
 
+// Deletando registro
+//app.delete('/selecoes/id:', (req, res) => {
+   // const id = req.params.id;
+    //const sql = "delete from selecoes where id=?;";
 
+    //conexao.query(sql, id, () => {
+      //  res.json({menssagem:"delete com sucesso"});
+    //});
+
+//});
 
 
 export default app;
-
